56c2dd60cf5552fd9a38eeda2103cc833a7a4171
keerthana-kanini <keerthana.kanini@gmail.com>
2023-06-30 13:27:55 +0530

    ModelClass Updated

--- Big_Bang_Assessment_2/Program.cs
+++ Big_Bang_Assessment_2/Program.cs
@@ -1,3 +1,6 @@
+using Big_Bang_Assessment_2.DBContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HospitalDPContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DoctorPatient")));
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 var app = builder.Build();
 
